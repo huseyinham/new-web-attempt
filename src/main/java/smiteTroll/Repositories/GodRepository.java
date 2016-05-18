1ba5e98b1171a17141b3de78ef7b624e8842c982
Huseyin Hamilton <huseyin.hamilton@travisperkins.co.uk>
2016-05-18 10:09:12 +0100

    added more tests and reroll feature

--- src/main/java/smiteTroll/Repositories/GodRepository.java
+++ src/main/java/smiteTroll/Repositories/GodRepository.java
@@ -7,22 +7,17 @@ import java.sql.*;
 
 public class GodRepository {
 
+    private ConnectionCreator connectionCreator = new ConnectionCreator();
+
     public God getNewGod() throws SQLException, ClassNotFoundException, IOException {
         Connection con = null;
         Statement stmt = null;
 
         try {
-            ConnectionCreator connectionCreator = new ConnectionCreator();
             con = connectionCreator.getConnection();
             stmt = con.createStatement();
-            PreparedStatement prepStmt = null;
-            if(1 == 1){ //clicked reroll button - then enter this SQL query else perform original
-                prepStmt = con.prepareStatement("SELECT * FROM god WHERE god_name != ? ORDER BY RAND() LIMIT 1");
-                prepStmt.setString(1, godName);
-            } else {
-                prepStmt = con.prepareStatement("SELECT * FROM god ORDER BY RAND() LIMIT 1");
-            }
-            ResultSet rs = prepStmt.executeQuery();
+            String query = "SELECT * FROM god ORDER BY RAND() LIMIT 1";
+            ResultSet rs = stmt.executeQuery(query);
             rs.next();
             String godName = rs.getString("god_name");
             String godType = rs.getString("god_type");
@@ -38,9 +33,27 @@ public class GodRepository {
         }
     }
 
-    public God reRoll(){
+    public God reRoll(God previousGod) throws SQLException, IOException, ClassNotFoundException {
+        Connection con = null;
+        PreparedStatement prepStmt = null;
 
+        try {
+            con = connectionCreator.getConnection();
+            prepStmt = con.prepareStatement("SELECT * FROM god WHERE god_name != ? ORDER BY RAND() LIMIT 1");
+            prepStmt.setString(1, previousGod.getGodName());
+            ResultSet rs = prepStmt.executeQuery();
+            rs.next();
+            String godName = rs.getString("god_name");
+            String godType = rs.getString("god_type");
+            return new God(godName, godType);
 
-        return God;
+        } finally {
+            if (prepStmt != null) {
+                prepStmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 }
---
+++ src/test/java/smiteTroll/GodRepositoryTest.java
@@ -0,0 +1,108 @@
+package smiteTroll;
+
+import org.junit.Assert;
+import org.junit.Test;
+import smiteTroll.Classes.God;
+import smiteTroll.Repositories.GodRepository;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GodRepositoryTest {
+
+    @Test
+    public void testRerollFunctionality() throws SQLException, IOException, ClassNotFoundException {
+        GodRepository godRepo = new GodRepository();
+        God previousGod = new God("Thanatos", "physical");
+        God selectedGod = godRepo.reRoll(previousGod);
+        List<God> listOfGods = getPotentialRerollableGods();
+        for (God god : listOfGods) {
+            System.out.println(selectedGod.getGodName());
+            System.out.println(god.getGodName());
+            //Assert.assertTrue(selectedGod.getGodName().contains(god.getGodName()));
+            //Assert.assertTrue(!selectedGod.getGodName().contains(previousGod.getGodName));
+        }
+    }
+
+    private List<God> getPotentialRerollableGods() {
+        List<God> listOfGods = new ArrayList<>();
+        listOfGods.add(new God("Agni", "magical"));
+        listOfGods.add(new God("Ah Muzen Cab", "physical"));
+        listOfGods.add(new God("Amaterasu", "physical"));
+        listOfGods.add(new God("Ah Puch", "magical"));
+        listOfGods.add(new God("Anhur", "physical"));
+        listOfGods.add(new God("Anubis", "magical"));
+        listOfGods.add(new God("Ao Kuang", "magical"));
+        listOfGods.add(new God("Aphrodite", "magical"));
+        listOfGods.add(new God("Apollo", "physical"));
+        listOfGods.add(new God("Arachne", "physical"));
+        listOfGods.add(new God("Ares", "magical"));
+        listOfGods.add(new God("Artemis", "physical"));
+        listOfGods.add(new God("Athena", "magical"));
+        listOfGods.add(new God("Awilix", "physical"));
+        listOfGods.add(new God("Bacchus", "magical"));
+        listOfGods.add(new God("Bakasura", "physical"));
+        listOfGods.add(new God("Bastet", "physical"));
+        listOfGods.add(new God("Bellona", "physical"));
+        listOfGods.add(new God("Cabrakan", "magical"));
+        listOfGods.add(new God("Chaac", "physical"));
+        listOfGods.add(new God("Change", "magical"));
+        listOfGods.add(new God("Chiron", "physical"));
+        listOfGods.add(new God("Chronos", "magical"));
+        listOfGods.add(new God("Cupid", "physical"));
+        listOfGods.add(new God("Fenrir", "physical"));
+        listOfGods.add(new God("Freya", "magical"));
+        listOfGods.add(new God("Geb", "magical"));
+        listOfGods.add(new God("Guan Yu", "physical"));
+        listOfGods.add(new God("Hades", "magical"));
+        listOfGods.add(new God("He Bo", "magical"));
+        listOfGods.add(new God("Hel", "magical"));
+        listOfGods.add(new God("Hercules", "physical"));
+        listOfGods.add(new God("Hou Yi", "physical"));
+        listOfGods.add(new God("Hun Batz", "physical"));
+        listOfGods.add(new God("Isis", "magical"));
+        listOfGods.add(new God("Janus", "magical"));
+        listOfGods.add(new God("Jing Wei", "physical"));
+        listOfGods.add(new God("Kali", "physical"));
+        listOfGods.add(new God("Khepri", "magical"));
+        listOfGods.add(new God("Kukulkan", "magical"));
+        listOfGods.add(new God("Kumbhakarna", "magical"));
+        listOfGods.add(new God("Loki", "physical"));
+        listOfGods.add(new God("Medusa", "physical"));
+        listOfGods.add(new God("Mercury", "physical"));
+        listOfGods.add(new God("Ne Zha", "physical"));
+        listOfGods.add(new God("Neith", "physical"));
+        listOfGods.add(new God("Nemesis", "physical"));
+        listOfGods.add(new God("Nox", "magical"));
+        listOfGods.add(new God("Nu Wa", "magical"));
+        listOfGods.add(new God("Odin", "physical"));
+        listOfGods.add(new God("Osiris", "physical"));
+        listOfGods.add(new God("Poseidon", "magical"));
+        listOfGods.add(new God("Ra", "magical"));
+        listOfGods.add(new God("Raijin", "magical"));
+        listOfGods.add(new God("Rama", "physical"));
+        listOfGods.add(new God("Ratatoskr", "physical"));
+        listOfGods.add(new God("Ravana", "physical"));
+        listOfGods.add(new God("Scylla", "magical"));
+        listOfGods.add(new God("Serqet", "physical"));
+        listOfGods.add(new God("Skadi", "physical"));
+        listOfGods.add(new God("Sobek", "magical"));
+        listOfGods.add(new God("Sol", "magical"));
+        listOfGods.add(new God("Sun Wukong", "physical"));
+        listOfGods.add(new God("Susano", "physical"));
+        listOfGods.add(new God("Sylvanus", "magical"));
+        listOfGods.add(new God("Thor", "physical"));
+        listOfGods.add(new God("Tyr", "physical"));
+        listOfGods.add(new God("Ullr", "physical"));
+        listOfGods.add(new God("Vamana", "physical"));
+        listOfGods.add(new God("Vulcan", "magical"));
+        listOfGods.add(new God("Xbalanque", "physical"));
+        listOfGods.add(new God("Xing Tian", "magical"));
+        listOfGods.add(new God("Ymir", "magical"));
+        listOfGods.add(new God("Zeus", "magical"));
+        listOfGods.add(new God("Zhong Kui", "magical"));
+        return listOfGods;
+    }
+}
